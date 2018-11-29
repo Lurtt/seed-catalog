c62b700cc79a643a689fd51d584bdaf23fdb47a0
Lukas Pospisil <lurttik@gmail.com>
2018-11-29 10:58:41 +0100

    init prisma and server

---
+++ src/index.ts
@@ -0,0 +1,78 @@
+import { prisma } from './generated/prisma-client'
+import { GraphQLServer } from 'graphql-yoga'
+
+const resolvers = {
+  Query: {
+    allPosts(parent, args, context) {
+      return context.prisma.posts()
+    },
+    publishedPosts(parent, args, context) {
+      return context.prisma.posts({ where: { published: true } })
+    },
+    post(parent, args, context) {
+      return context.prisma.post({ id: args.postId })
+    },
+    postsByUser(parent, args, context) {
+      return context.prisma
+        .user({
+          id: args.userId,
+        })
+        .posts()
+    },
+  },
+  Mutation: {
+    createDraft(parent, args, context) {
+      return context.prisma.createPost({
+        title: args.title,
+        author: {
+          connect: { id: args.userId },
+        },
+      })
+    },
+    publish(parent, args, context) {
+      return context.prisma.updatePost({
+        where: { id: args.postId },
+        data: { published: true },
+      })
+    },
+    createUser(parent, args, context) {
+      return context.prisma.createUser({ name: args.name })
+    },
+  },
+  User: {
+    posts(parent, args, context) {
+      return context.prisma
+        .user({
+          id: parent.id,
+        })
+        .posts()
+    },
+  },
+  Post: {
+    author(parent, args, context) {
+      return context.prisma
+        .post({
+          id: parent.id,
+        })
+        .author()
+    },
+  },
+}
+
+const options = {
+  port: 9000,
+  playground: '/playground',
+}
+
+const server = new GraphQLServer({
+  typeDefs: './src/schema.graphql',
+  resolvers,
+  context: {
+    prisma,
+  },
+})
+server.start(options, ({ port }) =>
+  console.log(
+    `Server started, listening on port ${port} for incoming requests.`,
+  ),
+)
